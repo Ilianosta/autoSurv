d6bd86a0ab6a2e95d829676288d9db9b89d3c7e8
Ilian Aron Ostapovich <ilian@pizia.com>
2022-09-20 09:25:13 -0300

    Initial commit

---
+++ Assets/_Game/Dev/_Scripts/KrownzEssentials.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.Krownz.Maths
+{
+    public class KrownzEssentials : MonoBehaviour
+    {
+    }
+}
+namespace com.Krownz._2D
+{
+    public class KrownzEssentials : MonoBehaviour
+    {
+        
+    }
+}
+namespace com.Krownz._3D
+{
+    public class KrownzEssentials : MonoBehaviour
+    {
+        
+    }
+}
